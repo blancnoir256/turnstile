c1bcb5b94a5df9bf4d5eaf253ee5edb49cb9be41
blancnoir256 <116483234+blancnoir256@users.noreply.github.com>
2025-12-08 02:17:17 +0900

    add: implement formula evaluation classes and update item retrieval

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional, List
+import tree
 
 app = FastAPI()
 
@@ -7,9 +9,23 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
+def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-class 
+
+class Formula:
+    input_string:str = ""                   # 入力
+    symbolic_representation_tree:tree.Node  # 公式な記号文として木構造にしたもの
+    tf_table = None                         # 真理値表
+    syntactic_derivation = None             # 統語論的方法による演繹の道筋
+
+    def __init__(self,val):
+        self.input_string = val
+        self.is_well_formed = False         # 入力は記号文か？
+        self.is_formal = False              # 入力は公式な記号文か？
+
+
+class ronri:
+    premises: List[Formula]
+    conclusion: Formula
---
+++ tree.py
@@ -0,0 +1,88 @@
+import re
+
+class Node:
+    def __init__(self):
+        raise NotImplementedError
+    def __repr__(self) -> str:
+        raise NotImplementedError
+    def evaluate(self, env):
+        raise NotImplementedError
+
+
+class Atom(Node):
+    def __init__(self, val):
+        self.value = val
+    def __repr__(self) -> str:
+        return f"{self.value}"
+    def evaluate(self, env):
+        return env[self.value]
+
+
+class Operator(Node):
+    def __init__(self, symbol, *args):
+        self.symbol = symbol
+        self.args = args
+
+    @property
+    def left(self) -> Node:
+        return self.args[0]
+    @property
+    def right(self) -> Node:
+        return self.args[1]
+    @property
+    def child(self) -> Node:
+        return self.args[0]
+
+    def __repr__(self):
+        if len(self.args) == 0:
+            return f"{self.symbol}"
+        if len(self.args) == 1:
+            return f" {self.symbol}{self.args[0]} "
+        if len(self.args) == 2:
+            return f" ({self.args[0]}{self.symbol}{self.args[1]}) "
+        else:
+            return f"({f' {self.symbol} '.join(map(str, self.args))})"
+
+    def evaluate(self, env):
+        return super().evaluate(env)
+
+
+class Not(Operator):
+    def __init__(self, child):
+        super().__init__("\u223C", child)
+
+    def evaluate(self, env):
+        return not self.child.evaluate(env)
+
+
+class And(Operator):
+    def __init__(self, left, right):
+        super().__init__("\u2227", left, right)
+
+    def evaluate(self, env):
+        return self.left.evaluate(env) and self.right.evaluate(env)
+
+
+class Or(Operator):
+    def __init__(self, left, right):
+        super().__init__("\u2228", left, right)
+    
+    def evaluate(self, env):
+        return self.left.evaluate(env) or self.right.evaluate(env)
+
+
+class Implies(Operator):
+    def __init__(self, left, right):
+        super().__init__("\u2192", left, right)
+
+    def evaluate(self, env):
+        return (not self.left.evaluate(env)) or self.right.evaluate(env)
+
+
+class Equivalence(Operator):
+    def __init__(self, left, right):
+        super().__init__("\u2194", left, right)
+
+    def evaluate(self, env):
+        return self.left.evaluate(env) == self.right.evaluate(env)
+
