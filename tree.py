961d2875261a53fb34ad52e27ede698b9aa249e4
bn256 <116483234+blancnoir256@users.noreply.github.com>
2025-12-09 11:58:53 +0900

    feat 表記をASCIIに統一

--- tree.py
+++ tree.py
@@ -37,9 +37,9 @@ class Operator(Node):
         if len(self.args) == 0:
             return f"{self.symbol}"
         if len(self.args) == 1:
-            return f" {self.symbol}{self.args[0]} "
+            return f"{self.symbol}{self.args[0]}"
         if len(self.args) == 2:
-            return f" ({self.args[0]}{self.symbol}{self.args[1]}) "
+            return f"({self.args[0]}{self.symbol}{self.args[1]})"
         else:
             return f"({f' {self.symbol} '.join(map(str, self.args))})"
 
@@ -49,7 +49,8 @@ class Operator(Node):
 
 class Not(Operator):
     def __init__(self, child):
-        super().__init__("\u223C", child)
+        # "~","\u223C" 但しここでは"~"
+        super().__init__(chr(0x7E), child)
 
     def evaluate(self, env):
         return not self.child.evaluate(env)
@@ -57,7 +58,8 @@ class Not(Operator):
 
 class And(Operator):
     def __init__(self, left, right):
-        super().__init__("\u2227", left, right)
+        # "∧","\u2227" 但しここでは"&"
+        super().__init__(chr(0x26), left, right)
 
     def evaluate(self, env):
         return self.left.evaluate(env) and self.right.evaluate(env)
@@ -65,7 +67,8 @@ class And(Operator):
 
 class Or(Operator):
     def __init__(self, left, right):
-        super().__init__("\u2228", left, right)
+        # "∨","\u2228" 但しここでは"|"
+        super().__init__(chr(0x7C), left, right)
     
     def evaluate(self, env):
         return self.left.evaluate(env) or self.right.evaluate(env)
@@ -73,7 +76,8 @@ class Or(Operator):
 
 class Implies(Operator):
     def __init__(self, left, right):
-        super().__init__("\u2192", left, right)
+        # "\u2192" 但しここでは"->"
+        super().__init__(chr(0x2D)+chr(0x3E), left, right)
 
     def evaluate(self, env):
         return (not self.left.evaluate(env)) or self.right.evaluate(env)
@@ -81,7 +85,8 @@ class Implies(Operator):
 
 class Equivalence(Operator):
     def __init__(self, left, right):
-        super().__init__("\u2194", left, right)
+        # "\u2194" 但しここでは"<->"
+        super().__init__(chr(0x3C)+chr(0x2D)+chr(0x3E), left, right)
 
     def evaluate(self, env):
         return self.left.evaluate(env) == self.right.evaluate(env)
