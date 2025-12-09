7d5c2d2b2b346e4b9ac0eb1a69453414934da1c0
bn256 <116483234+blancnoir256@users.noreply.github.com>
2025-12-09 12:24:24 +0900

    add test

--- test.py
+++ test.py
@@ -4,3 +4,10 @@ from tree import Node, Atom, Operator, Not, And, Or, Implies, Equivalence
 test = And(Implies(Atom("P"),Atom("Q")),Or(Not(Atom("P")),Equivalence(Atom("Q"),Atom("R"))))
 
 print(test)
+
+from tokenizer import LexerGenerator
+
+test_text = "(P->Q)&(~P|(Q<->R))"
+test = LexerGenerator(test_text).tokens
+print(test)
+
